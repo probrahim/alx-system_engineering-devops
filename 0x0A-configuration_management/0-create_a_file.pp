fda50be9c1eae9b82d17e377e1a2c320154117cf
probrahim <ibrahimhassani674@gmail.com>
2024-05-07 18:11:05 +0200

    task 00

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,10 @@
 # creation de file 
 
-file { 'tmp/school':
- content => 'I love Puppet',
- mode  => '0744',
- owner => 'www-data',
- group => 'www-data',
-
-
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
-
